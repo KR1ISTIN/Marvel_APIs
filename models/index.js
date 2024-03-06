d3192bc7d25f811c918a932e362fd826ade091a9
KR1ISTIN <kristin.desalme@gmail.com>
2024-03-05 22:01:36 -0600

    added relation between tables

--- models/index.js
+++ models/index.js
@@ -2,6 +2,25 @@ const ComicBook = require('./ComicBooks');
 const Movies = require('./Movies');
 const TvShows = require('./TvShows');
 
+ComicBook.hasMany(Movies, {
+    foreignKey: 'comicBook_id',
+    onDelete: 'CASCADE',
+});
+
+Movies.belongsTo(ComicBook, {
+    foreignKey: 'comicBook_id'
+});
+
+
+
+ComicBook.hasMany(TvShows, {
+    foreignKey: 'comicBook_id',
+    onDelete: 'CASCADE',
+});
+
+TvShows.belongsTo(ComicBook, {
+    foreignKey: 'comicBook_id'
+});
 
 
 
